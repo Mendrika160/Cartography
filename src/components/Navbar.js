c804041e329fde3cb525c3498647abeb48f61189
Mendrika160 <rahelisonhajaina@gmail.com>
2023-01-04 18:28:21 +0100

    refactor navbar component

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,26 +1,6 @@
-import React ,{useRef}from 'react'
-import {ContextMenu } from 'react-tiny-contextmenu'
+import React from 'react'
 
 export default function Navbar() {
-  const theRef = useRef(null)
-
-  const items = [
-    {
-        children: 'One Button',
-    },
-    {
-        children: (
-            <div>
-                <button>two</button>
-                <button>button</button>
-            </div>
-    ),
-    },
-
-]
-const contextMenu = async (e) => {
-  console.log(e)
-}
   
   return (
     <>
@@ -28,8 +8,7 @@ const contextMenu = async (e) => {
             <div className="container d-flex flex-wrap justify-content-center">
             <div className="nav-logo d-flex align-items-center mb-3 mb-lg-0 me-lg-auto text-dark text-decoration-none">Test</div>
             </div>
-            <button onContextMenu={contextMenu} ref={theRef} >Test</button>
-            <ContextMenu target={theRef} items={items}></ContextMenu>
+      
         </header>
 
     
